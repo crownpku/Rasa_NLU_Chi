757c2f2ef317a8dc232395bfa62ad7728c057d6c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 18:35:13 +0000

    Adding tarfile member sanitization to extractall()

--- rasa_nlu/persistor.py
+++ rasa_nlu/persistor.py
@@ -132,7 +132,26 @@ class Persistor(object):
         # type: (Text, Text) -> None
 
         with tarfile.open(compressed_path, "r:gz") as tar:
-            tar.extractall(
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, target_path)
                 target_path)  # project dir will be created if it not exists
 
 
